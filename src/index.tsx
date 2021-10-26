c67fa16307b8cb4172cd649f91d476dc58e55e05
ArtemEfimovich <artemfender@gmail.com>
2021-10-26 23:00:44 +0300

    Login Log out Formik

--- src/index.tsx
+++ src/index.tsx
@@ -5,13 +5,16 @@ import App from './app/App';
 import reportWebVitals from './reportWebVitals';
 import {Provider} from 'react-redux';
 import {store} from './app/store';
-import {BrowserRouter} from "react-router-dom";
+import {BrowserRouter, HashRouter} from "react-router-dom";
 
 ReactDOM.render(
     <React.StrictMode>
-        <Provider store={store}>
-            <BrowserRouter><App/></BrowserRouter>
-        </Provider>
+        <HashRouter>
+            <Provider store={store}>
+               <App/>
+            </Provider>
+        </HashRouter>
+
     </React.StrictMode>,
     document.getElementById('root')
 );
